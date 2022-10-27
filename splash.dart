571467fefa3008436d59f8479fdc953b421a0afd
yashjain0827 <83298359+yashjain0827@users.noreply.github.com>
2022-10-27 12:26:48 +0530

    Add files via upload

---
+++ splash.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class splash extends StatelessWidget {
+  const splash({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Container(
+        child: Text('splash screen'),
+      ),
+    );
+  }
+}
